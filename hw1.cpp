2384e1870b31ce4f65ae4f6ae0e9048539f814a8
wilsonkayla <kaylazwilsonbusiness@gmail.com>
2026-02-07 23:36:47 -0500

    hw1

---
+++ hw1.cpp
@@ -0,0 +1,24 @@
+/*
+Author: Kayla Wilson
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab1A
+
+This file askes the user to put in a year
+so so the user can find out if its a leap or common
+year. In the program there are 2 of each.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name[3];
+    for( int i=0; i<3; i++){
+        cin >> name[i];
+        cout << name[i] <<endl; 
+    }
+
+    return 0; 
+}
