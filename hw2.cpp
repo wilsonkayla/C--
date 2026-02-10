1a346cc60b24ed2bcc213d771576ef7e388f052c
wilsonkayla <kaylazwilsonbusiness@gmail.com>
2026-02-10 17:05:14 -0500

    hw 2

---
+++ hw2.cpp
@@ -0,0 +1,30 @@
+/*
+Author: Kayla Wilson
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab1A
+
+This file askes the user to put in a year
+so so the user can find out if its a leap or common
+year. In the program there are 2 of each.
+*/
+
+int main(){
+    int gas;
+    int fuel;
+    int price;
+    cout << "Enter number of gallons of gas in the tank: "<< endl;
+    cin >> gas;
+    cout << "Enter the fuel efficiency in miles per gallon: " << endl;
+    cin >> fuel;
+    cout << " Enter price of gas per gallon: " << endl;
+    cin >> price;
+
+    double mpg = (100/fuel) * price;
+    double distance = gas*fuel;
+    cout << "Cost per 100 miles is $ "<< mpg << endl;
+    cout << "The car can go " << distance << "miles with gas in the tank" << endl; 
+
+    return 0;
+}
+
