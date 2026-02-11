d032476b01d61be0dd7eed56e7976c16565e5fad
wilsonkayla <kaylazwilsonbusiness@gmail.com>
2026-02-11 16:33:26 -0500

    cnew things

---
+++ lab3.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+ using namespace std;
+
+ int main(){
+    ifstream fin("Current_Reservoir_Levels.tsv");
+
+    if (fin.fail()){
+        cerr << "File cannot be opened to reading." << endl;
+        exit(1);
+    }
+    fin.ignore(INT_MAX, '\n'); 
+
+    string junk, date, eastSt, eastE1, westSt,westE1; 
+
+    while(fin >> date >> eastSt >> eastE1 >> westSt >> westE1){
+
+        cout << date << " " << eastSt << endl;
+
+    }
+
+    fin.close();
+
+ }
